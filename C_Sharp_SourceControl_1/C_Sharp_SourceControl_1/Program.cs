99e6bd6cc0fad3c0ded59c62d33aaea5946a0b55
Conlan <cgotzion@gmail.com>
2021-06-15 13:53:10 -0500

    Added Stuff

--- C_Sharp_SourceControl_1/C_Sharp_SourceControl_1/Program.cs
+++ C_Sharp_SourceControl_1/C_Sharp_SourceControl_1/Program.cs
@@ -7,10 +7,11 @@ namespace C_Sharp_SourceControl_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Andrew, BC, idk");
+            Console.WriteLine("Andrew, BC, i");
             Console.WriteLine("Joel, Purdue, Fornite Llama");
             Console.WriteLine("Anna, BC, a dead plant :(");
-            Console.WriteLine("Conlan, UW Madison, Food that gave me food poisioning");
+            Console.WriteLine("Conlan, UW Madison, Food that gave me food poisioning, Gift money");
+            
 
         }
     }
