f305ec07d7b0e26f06a8cdbcc9b1148707cd060c
Andrew Prudlow <22prudla1@elmbrookstudents.org>
2021-06-15 13:52:18 -0500

    yes2

--- C_Sharp_SourceControl_1/C_Sharp_SourceControl_1/Program.cs
+++ C_Sharp_SourceControl_1/C_Sharp_SourceControl_1/Program.cs
@@ -9,8 +9,9 @@ namespace C_Sharp_SourceControl_1
             Console.WriteLine("Hello World!");
             Console.WriteLine("Andrew, BC, idk");
             Console.WriteLine("Joel, Purdue, Fornite Llama");
-            Console.WriteLine("Anna, BC, a dead plant :(");
+            Console.WriteLine("Anna, BC, dead plant :(");
             Console.WriteLine("Conlan, UW Madison, Food that gave me food poisioning");
+            Console.WriteLine("ummmmmmmmmmmmm idk");
 
         }
     }
